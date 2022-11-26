5959ae196ccdde07eab2f84889fe5d13b328bad2
ayaznoori <ayaz.noori15@gmail.com>
2022-11-26 12:45:45 +0530

    server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
-server.use('/api', router)
+server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
 })
